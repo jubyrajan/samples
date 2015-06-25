33b8dbb011771c585f66404883626f86db9916ad
Juby Rajan <jubyrajan@gmail.com>
2015-06-25 17:44:25 +0530

    Adding Hello, World Java class

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Basic Hello, World class without package.
+ * @author Juby Rajan
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	}
+}
